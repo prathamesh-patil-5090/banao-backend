8d9d257703064e60aca4a99914250e045e648db6
Prathamesh Patil <134536755+prathamesh-patil-5090@users.noreply.github.com>
2025-01-17 23:33:15 +0530

    Update index.js

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ app.use(morgan("common"));
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors({
-  origin: ["http://localhost:3000", "http://localhost:3001"],
+  origin: ["http://localhost:3000", "http://localhost:3001", "https://banao-jo4i.onrender.com"],
   methods: ["GET", "POST", "PATCH", "DELETE", "PUT"],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true,
