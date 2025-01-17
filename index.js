b944ba07528c15961ad5ef247d5636c7bc3718be
Prathamesh Patil <134536755+prathamesh-patil-5090@users.noreply.github.com>
2025-01-17 23:40:15 +0530

    Update index.js

--- index.js
+++ index.js
@@ -35,7 +35,7 @@ app.use(cors({
     "http://localhost:3001",
     "https://banao-jo4i.onrender.com"
   ],
-  methods: ["GET", "POST", "PATCH", "DELETE", "PUT"],
+  methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true,
 }));
